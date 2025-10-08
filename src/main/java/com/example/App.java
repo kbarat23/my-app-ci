55bc0478cc2cf69823d528f9a1e87be83653b13e
Karim Baratli <baraka01@uqo.ca>
2025-10-08 09:55:25 -0400

    ajout du fichier .gitignore

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello CI/CD !");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
